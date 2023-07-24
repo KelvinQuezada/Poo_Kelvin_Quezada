99e50ff5ab7804a6d267ff16ada501743ba37bb7
KelvinQuezada <93734023+KelvinQuezada@users.noreply.github.com>
2023-07-24 16:39:54 -0500

    tercera unidad

---
+++ Tercera_Unidad/Poo_U3_P1_CRUD/src/Controlador/conexion.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controlador;
+
+import Principal.Form_estudiante;
+
+public class conexion {
+    public static Form_estudiante ventana=new Form_estudiante();
+    public static void mostrar(){ventana.setVisible(true);}
+    
+}
---
+++ Tercera_Unidad/Poo_U3_P1_CRUD/src/poo_u3_p1_crud/Poo_U3_P1_CRUD.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package poo_u3_p1_crud;
+
+import Principal.Form_estudiante;
+
+/**
+ *
+ * @author ESPE
+ */
+public class Poo_U3_P1_CRUD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+    }
+    
+}
